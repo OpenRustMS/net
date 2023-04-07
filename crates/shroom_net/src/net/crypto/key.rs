73cbbad53a1a24440e9f529fb6f7509777167d97
Jonas <Jonas-Zunker@web.de>
2023-04-07 06:18:31 +0200

    Update key paths

--- crates/shroom_net/src/net/crypto/key.rs
+++ crates/shroom_net/src/net/crypto/key.rs
@@ -1,5 +1,5 @@
 use super::{AES_KEY_LEN, RoundKey};
 
-pub const DEFAULT_SHUFFLE_KEY: &[u8; 256] = include_bytes!("../../../../../default_keys/shuffle_key.bin");
-pub const DEFAULT_AES_KEY: &[u8; AES_KEY_LEN] = include_bytes!("../../../../../default_keys/aes_key.bin");
-pub const DEFAULT_INIT_IG_SEED: RoundKey = RoundKey(*include_bytes!("../../../../../default_keys/initial_round_key.bin"));
+pub const DEFAULT_SHUFFLE_KEY: &[u8; 256] = include_bytes!("default_keys/shuffle_key.bin");
+pub const DEFAULT_AES_KEY: &[u8; AES_KEY_LEN] = include_bytes!("default_keys/aes_key.bin");
+pub const DEFAULT_INIT_IG_SEED: RoundKey = RoundKey(*include_bytes!("default_keys/initial_round_key.bin"));
